fe2c0cf76de4655b8c6ded39c2a92c5a4daf9575
austinschroeder <austinsoundy@gmail.com>
2021-09-09 11:38:33 -0700

    Adding touches to CardTop font style

--- src/components/CardTop.js
+++ src/components/CardTop.js
@@ -5,8 +5,8 @@ function CardTop() {
     <div className="head">
       <div className="container-top">
         <h1>Unleash the Power of Your Contracts</h1>
-        <p>Write better legal agreements, close deals faster, and understand every aspect of every contract, all with LinkSquares.</p>
-        <p>Contracts power your business. Let LinkSquares super-power your contracts.</p>
+        <p className="header-text">Write better legal agreements, close deals faster, and understand every aspect of every contract, all with LinkSquares.</p>
+        <p className="header-text">Contracts power your business. Let LinkSquares super-power your contracts.</p>
         <a className="two-minute-overview">▶ 2 minute overview</a>
         <a className="request-a-demo">Request a demo</a>
       </div>
