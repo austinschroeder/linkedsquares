4a083dbfb94909680349d57bc8305e0d182b4166
austinschroeder <austinsoundy@gmail.com>
2021-09-08 19:54:45 -0700

    Added image gradient

--- src/components/CardTwo.js
+++ src/components/CardTwo.js
@@ -1,34 +1,68 @@
-import './CardTwo.css'
+import "./CardTwo.css";
 
 function CardTwo() {
-  return(
+  return (
     <div className="card-two-container">
-      <img className="woman-coffee" src="https://linksquares.com/wp-content/uploads/logo-section-bg.jpg" alt="" />
+      <div className="woman-coffee-container">
+        <div className="img-gradient">
+          <div className="img-gradient-text">We manage the contracts of some of the world's best companies</div>
+          <img
+            className="woman-coffee"
+            src="https://linksquares.com/wp-content/uploads/logo-section-bg.jpg"
+            alt=""
+          />
+        </div>
+      </div>
 
       <div className="companies-container">
         <div className="companies-row-one">
-          <img src="https://linksquares.com/wp-content/uploads/Wayfair_logo_with_tagline-1-e1623703041593.png" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/draftkings-logo-1.jpg" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/pegasystems-inc-vector-logo-1.jpg" alt="" />
+          <img
+            src="https://linksquares.com/wp-content/uploads/Wayfair_logo_with_tagline-1-e1623703041593.png"
+            alt=""
+          />
+          <img
+            src="https://linksquares.com/wp-content/uploads/draftkings-logo-1.jpg"
+            alt=""
+          />
+          <img
+            src="https://linksquares.com/wp-content/uploads/pegasystems-inc-vector-logo-1.jpg"
+            alt=""
+          />
         </div>
 
         <div className="companies-row-two">
-          <img src="https://linksquares.com/wp-content/uploads/Arbor_Pharmaceuticals_Logo-1.jpg" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/fitbit-new-logo-header-1.jpg" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/Twilio_logo_fzjbxTI-1.jpg" alt="" />
+          <img
+            src="https://linksquares.com/wp-content/uploads/Arbor_Pharmaceuticals_Logo-1.jpg"
+            alt=""
+          />
+          <img
+            src="https://linksquares.com/wp-content/uploads/fitbit-new-logo-header-1.jpg"
+            alt=""
+          />
+          <img
+            src="https://linksquares.com/wp-content/uploads/Twilio_logo_fzjbxTI-1.jpg"
+            alt=""
+          />
         </div>
 
         <div className="companies-row-three">
-          <img src="https://linksquares.com/wp-content/uploads/BottomlineLogo-1.jpg" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/igloo-coolers-vector-logo-1.jpg" alt="" />
+          <img
+            src="https://linksquares.com/wp-content/uploads/BottomlineLogo-1.jpg"
+            alt=""
+          />
+          <img
+            src="https://linksquares.com/wp-content/uploads/igloo-coolers-vector-logo-1.jpg"
+            alt=""
+          />
           <img src="" alt="" />
-          <img src="https://linksquares.com/wp-content/uploads/VMware-logo-1.jpg" alt="" />
+          <img
+            src="https://linksquares.com/wp-content/uploads/VMware-logo-1.jpg"
+            alt=""
+          />
         </div>
-
       </div>
     </div>
-  
-  )
+  );
 }
 
 export default CardTwo;
--- src/components/TopAdvert.js
+++ src/components/TopAdvert.js
@@ -1,13 +1,14 @@
-import './TopAdvert.css'
+import "./TopAdvert.css";
 
 function TopAdvert() {
-  return(
-    
+  return (
     <div className="topAdvert">
-      <p className="topAdvert-text">LinkSquares has raised a $40M Series B financing round to continue pioneering advanced AI-powered CLM. Watch the webinar to learn more.</p>
+      <p>
+        LinkSquares has raised a $40M Series B financing round to continue
+        pioneering advanced AI-powered CLM. Watch the webinar to learn more.
+      </p>
     </div>
-  
-  )
+  );
 }
 
 export default TopAdvert;
